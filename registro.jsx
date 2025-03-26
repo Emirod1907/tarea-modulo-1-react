f7423bf854fc811d8a78d4cecaca2bc0d69301db
Emirod1907 <emi.electro2012@gmail.com>
2025-03-25 23:46:29 -0300

    primer commit

---
+++ registro.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./style.css";
+
+const Registro = () => {
+  return (
+    <div className="container">
+      <div className="form-container">
+        <h2 className="title">Registro</h2>
+        <form>
+          <div className="input-group">
+            <label>Nombre</label>
+            <input type="text" />
+          </div>
+          <div className="input-group">
+            <label>Apellido</label>
+            <input type="text" />
+          </div>
+          <div className="input-group">
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div className="input-group">
+            <label>Teléfono</label>
+            <input type="tel" />
+          </div>
+          <div className="input-group">
+            <label>Contraseña</label>
+            <input type="password" />
+          </div>
+          <div className="input-group">
+            <label>Confirmar Contraseña</label>
+            <input type="password" />
+          </div>
+          <button type="submit" className="submit-button">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Registro;
